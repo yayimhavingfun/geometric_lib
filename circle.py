0fb3085062b4c1f9649bfa9791dbd3abbb8b6e6a
Sol Trembach <antrembatch@gmail.com>
2024-10-01 19:58:37 +0300

    added description

--- circle.py
+++ circle.py
@@ -1,10 +1,10 @@
 import math
 
-
+'''accepts r - radius, returns area of a circle'''
 def area(r):
     return math.pi * r * r
 
-
+'''accepts r - radius, returns perimeter of a circle'''
 def perimeter(r):
     return 2 * math.pi * r
 
---
+++ triangle.py
@@ -0,0 +1,7 @@
+''' accepts arguments a - side, h - height, returns area of equilateral triangle'''
+def area(a, h):
+    return a * h / 2
+''' accepts arguments a, b, c - sides, returns perimeter of any triangle'''
+def perimeter(a, b, c):
+    return a + b + c
+
