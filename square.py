4374f079d7d457b2adba08ea781ead4a4b108095
yayimhavingfun <106334546+yayimhavingfun@users.noreply.github.com>
2024-10-18 20:30:01 +0300

    Update square.py

--- square.py
+++ square.py
@@ -1,7 +1,7 @@
-''' accepts a - side, returns area of a square'''
+#accepts a - side, returns area of a square
 def area(a):
     return a * a
 
-''' accepts a - side, returns perimeter of a square'''
+#accepts a - side, returns perimeter of a square
 def perimeter(a):
     return 4 * a
