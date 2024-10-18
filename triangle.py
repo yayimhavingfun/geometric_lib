4606162fe57c8fceb4658a6c10ccbdd048dd9829
yayimhavingfun <106334546+yayimhavingfun@users.noreply.github.com>
2024-10-18 20:37:14 +0300

    Update triangle.py

--- triangle.py
+++ triangle.py
@@ -1,8 +1,11 @@
 import math
-#accepts arguments a - side, h - height, returns area of a triangle
-def area(a, h):
-    return math.sqrt(p * (p-a) * (p-b) * (p-c))`
+
 #accepts arguments a, b, c - sides, returns perimeter of a triangle
 def perimeter(a, b, c):
     return a + b + c
+#accepts arguments a - side, h - height, returns area of a triangle
+def area(a, b, c):
+    p = perimeter(a, b, c) / 2
+    return math.sqrt(p * (p-a) * (p-b) * (p-c))`
+
 
