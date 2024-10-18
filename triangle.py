c447858b1aa311124a5ecaf7ad7e09bd37396ec1
yayimhavingfun <106334546+yayimhavingfun@users.noreply.github.com>
2024-10-18 20:26:14 +0300

    Update triangle.py

--- triangle.py
+++ triangle.py
@@ -1,7 +1,7 @@
-''' accepts arguments a - side, h - height, returns area of equilateral triangle'''
+''' accepts arguments a - side, h - height, returns area of a triangle'''
 def area(a, h):
     return a * h / 2
-''' accepts arguments a, b, c - sides, returns perimeter of any triangle'''
+''' accepts arguments a, b, c - sides, returns perimeter of a triangle'''
 def perimeter(a, b, c):
     return a + b + c
 
